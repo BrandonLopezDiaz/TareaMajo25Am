d93db34d10e54bf0a826111cc3403ae4ae7aa564
Brandon <brandon.lopes.diaz@gmail.com>
2023-02-10 11:17:47 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto25AM/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace Proyecto25AM
+{
+    public class StartUp
+    {
+        public StartUp(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get;}
+
+        public void ConfigureServices(IServiceCollection service)
+        {
+            service.AddControllers();
+            service.AddEndpointsApiExplorer();
+            service.AddSwaggerGen();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
